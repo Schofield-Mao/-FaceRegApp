9eeb54460e753c272988b1254242f7b84060837b
a_Mao <759082478@qq.com>
2020-04-13 15:42:11 +0800

    [feature]add epression detect:suprise\angry\happy\calm

--- app/src/main/java/com/example/faceregapp/MainActivity.java
+++ app/src/main/java/com/example/faceregapp/MainActivity.java
@@ -25,7 +25,8 @@ import org.opencv.core.Size;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.imgproc.Imgproc;
-import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.ml.CvSVM;
+import org.opencv.ml.CvStatModel;
 
 import android.app.Activity;
 import android.content.Context;
@@ -116,19 +117,6 @@ public class MainActivity extends Activity implements View.OnTouchListener ,CvCa
         imgView = (ImageView) findViewById(R.id.img_view);
         imgView.setOnTouchListener(this);
 
-
-//        final String targetPath = Constants.getFaceShapeModelPath();
-//        if (!new File(targetPath).exists()) {
-//            runOnUiThread(new Runnable() {
-//                @Override
-//                public void run() {
-//                    Toast.makeText(MainActivity.this, "Copy landmark model to " + targetPath, Toast.LENGTH_SHORT).show();
-//                }
-//            });
-//            Log.d(TAG, "oncreate: copy: "+Constants.getFaceShapeModelPath());
-            //FileStorageHelper.copyFilesFromRaw(this, R.raw.shape_predictor_68_face_landmarks, targetPath);
-//        }
-//        Log.d(TAG, "oncreate: copy done: "+Constants.getFaceShapeModelPath());
     }
 
     @Override
@@ -172,62 +160,63 @@ public class MainActivity extends Activity implements View.OnTouchListener ,CvCa
         mRgba = inputFrame.rgba();
         mGray = inputFrame.gray();
 
-        if (mAbsoluteFaceSize == 0) {
-            int height = mGray.rows();
-            if (Math.round(height * mRelativeFaceSize) > 0) {
-                mAbsoluteFaceSize = Math.round(height * mRelativeFaceSize);
-            }
-        }
-
-        MatOfRect faces = new MatOfRect();
-        if (mFaceDetector.getDetecotr() != null) {
-            mFaceDetector.getDetecotr().detectMultiScale(mGray, faces, 1.1, 3, 2,
-                    new Size(mAbsoluteFaceSize, mAbsoluteFaceSize), new Size());
-        }
+//        if (mAbsoluteFaceSize == 0) {
+//            int height = mGray.rows();
+//            if (Math.round(height * mRelativeFaceSize) > 0) {
+//                mAbsoluteFaceSize = Math.round(height * mRelativeFaceSize);
+//            }
+//        }
+//
+//        MatOfRect faces = new MatOfRect();
+//        if (mFaceDetector.getDetecotr() != null) {
+//            mFaceDetector.getDetecotr().detectMultiScale(mGray, faces, 1.1, 3, 2,
+//                    new Size(mAbsoluteFaceSize, mAbsoluteFaceSize), new Size());
+//        }
 //        Rect[] facesArray = faces.toArray();
 //        for (int i = 0; i < facesArray.length; i++) {
 //            Core.rectangle(mRgba, facesArray[i].tl(), facesArray[i].br(), FACE_RECT_COLOR, 3);
 //        }
 
-        Point temp = new Point(1,1);
-        Core.putText(mRgba,"happy: ", temp, FONT_HERSHEY_SIMPLEX, 3, FACE_RECT_COLOR);
         return mRgba;
     }
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        Toast.makeText(this,"you touch me", 1).show();
-        if(isViewStart){
-            try{
-                Mat img = mRgba;
-                if(img.cols() > 0 && img.rows() > 0) {
-                    processImage(img);
-                    Bitmap bitmapTemp = Bitmap.createBitmap(img.cols(), img.rows(), Bitmap.Config.ARGB_8888);
-                    Utils.matToBitmap(img, bitmapTemp);
-                    imgView.setImageBitmap(bitmapTemp);
-                    imgView.setVisibility(ImageView.VISIBLE);
-                    mOpenCvCameraView.disableView();
-                    mOpenCvCameraView.setVisibility(View.INVISIBLE);
-                    isViewStart = !isViewStart;
-                    Log.d(TAG, "ouTouch: stop on preview");
+
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+            Toast.makeText(this, "you touch me", 1).show();
+            if (isViewStart) {
+                try {
+                    Mat img = mRgba.clone();
+                    mRgba.copyTo(img);
+                    if (img.cols() > 0 && img.rows() > 0) {
+
+                        processImage(img);
+                        Bitmap bitmapTemp = Bitmap.createBitmap(img.cols(), img.rows(), Bitmap.Config.ARGB_8888);
+                        Utils.matToBitmap(img, bitmapTemp);
+                        imgView.setImageBitmap(bitmapTemp);
+                        imgView.setVisibility(ImageView.VISIBLE);
+                        mOpenCvCameraView.disableView();
+                        mOpenCvCameraView.setVisibility(View.INVISIBLE);
+                        isViewStart = !isViewStart;
+                        Log.d(TAG, "ouTouch: stop on preview");
+                    }
+                } catch (Exception ex) {
+                    Log.e(TAG, "onTouch: " + ex.getMessage());
                 }
-            }catch (Exception ex){
-                Log.e(TAG, "onTouch: "+ex.getMessage());
+            } else {
+                mOpenCvCameraView.enableView();
+                imgView.setVisibility(ImageView.INVISIBLE);
+                mOpenCvCameraView.setVisibility(View.VISIBLE);
+                isViewStart = !isViewStart;
+                Log.d(TAG, "ouTouch: set on preview");
             }
-        }else {
-            mOpenCvCameraView.enableView();
-            imgView.setVisibility(ImageView.INVISIBLE);
-            mOpenCvCameraView.setVisibility(View.VISIBLE);
-            isViewStart = !isViewStart;
-            Log.d(TAG, "ouTouch: set on preview");
+
         }
-
-
         return true;
     }
 
     private Mat processImage(Mat img){
-        int resizeRatio = 1;
         FaceDet faceDet = new FaceDet(Constants.getFaceShapeModelPath());
         Bitmap bitmapTemp = Bitmap.createBitmap(img.cols(), img.rows(), Bitmap.Config.ARGB_8888);
         Utils.matToBitmap(img, bitmapTemp);
@@ -235,27 +224,93 @@ public class MainActivity extends Activity implements View.OnTouchListener ,CvCa
         long now = System.currentTimeMillis();
         List<VisionDetRet> results = faceDet.detect(bitmapTemp);
         Log.d(TAG, "processImage: cost: "+(System.currentTimeMillis()-now));
+
         for (final VisionDetRet ret : results) {
             String label = ret.getLabel(); // If doing face detection, it will be 'Face'
             Log.d(TAG, "processImage: "+label);
-            int rectLeft = ret.getLeft();
-            int rectTop= ret.getTop();
-            int rectRight = ret.getRight();
-            int rectBottom = ret.getBottom();
-            Point lt = new Point();
-            Point br = new Point();
-            lt.x = rectLeft;
-            lt.y = rectTop;
-            br.x = rectRight;
-            br.y = rectBottom;
+            Point lt = new Point(ret.getLeft(), ret.getTop());
+            Point br = new Point(ret.getRight(), ret.getBottom());
             Log.d(TAG, "processImage: lt: "+lt.toString()+" br: "+br.toString());
             ArrayList<android.graphics.Point> landmarks = ret.getFaceLandmarks();
-            for (android.graphics.Point point : landmarks) {
-                Point temp = new Point(point.x * resizeRatio, point.y * resizeRatio);
-                //Log.d(TAG, "processImage: landmark: "+temp.toString());
-                Core.circle(img, temp,3,FACE_RECT_COLOR, 6);
+
+            for (int i=0;i<landmarks.size();i++) {
+                Point temp = new Point(landmarks.get(i).x, landmarks.get(i).y);
+                //Core.circle(img, temp,3,FACE_RECT_COLOR, 6);
+                Core.putText(img, Integer.toString(i), temp, FONT_HERSHEY_SIMPLEX, 0.5, FACE_RECT_COLOR,2);
+            }
+
+            Point explocat = new Point(100, 100);
+            if(isSuprise(landmarks)) {
+                Core.putText(img, "suprise! ", explocat, FONT_HERSHEY_SIMPLEX, 5, FACE_RECT_COLOR, 10);
+            }else if(isAngry(landmarks)) {
+                Core.putText(img, "angry! ", explocat, FONT_HERSHEY_SIMPLEX, 5, FACE_RECT_COLOR, 10);
+            }else if(isHappy(landmarks)) {
+                Core.putText(img, "happy! ", explocat, FONT_HERSHEY_SIMPLEX, 5, FACE_RECT_COLOR, 10);
+            }else {
+                Core.putText(img, "calm! ", explocat, FONT_HERSHEY_SIMPLEX, 5, FACE_RECT_COLOR, 10);
             }
         }
         return img;
     }
+
+    boolean isSuprise(ArrayList<android.graphics.Point> landmakrs){
+        //suprise:60 62 64 66
+        android.graphics.Point p1,p2;
+        p1 = landmakrs.get(60);
+        p2 = landmakrs.get(62);
+        Log.d(TAG, "isSuprise k1: "+(float)(p2.y-p1.y)/(p2.x-p1.x)+" p1: "+p1.toString()+" p2: "+p2.toString());
+        if((float)(p2.y-p1.y)/(p2.x-p1.x)>-0.15){
+            return false;
+        }
+        p1 = landmakrs.get(62);
+        p2 = landmakrs.get(64);
+        Log.d(TAG, "isSuprise k2: "+(p2.y-p1.y)/(p2.x-p1.x)+" p1: "+p1.toString()+" p2: "+p2.toString());
+        if((float)(p2.y-p1.y)/(p2.x-p1.x)<0.15){
+            return false;
+        }
+        p1 = landmakrs.get(64);
+        p2 = landmakrs.get(66);
+        Log.d(TAG, "isSuprise k3: "+(p2.y-p1.y)/(p2.x-p1.x)+" p1: "+p1.toString()+" p2: "+p2.toString());
+        if((float)(p2.y-p1.y)/(p2.x-p1.x)>-0.15){
+            return false;
+        }
+        return true;
+    }
+
+    boolean isHappy(ArrayList<android.graphics.Point> landmakrs){
+        //suprise:48,54,57
+        android.graphics.Point p1,p2;
+        p1 = landmakrs.get(48);
+        p2 = landmakrs.get(57);
+        Log.d(TAG, "isHappy k1: "+(float)(p2.y-p1.y)/(p2.x-p1.x)+" p1: "+p1.toString()+" p2: "+p2.toString());
+        if((float)(p2.y-p1.y)/(p2.x-p1.x)<0.15){
+            return false;
+        }
+        p1 = landmakrs.get(57);
+        p2 = landmakrs.get(54);
+        Log.d(TAG, "isHappy k2: "+(float)(p2.y-p1.y)/(p2.x-p1.x)+" p1: "+p1.toString()+" p2: "+p2.toString());
+        if((float)(p2.y-p1.y)/(p2.x-p1.x)>-0.15){
+            return false;
+        }
+        return true;
+    }
+
+    boolean isAngry(ArrayList<android.graphics.Point> landmakrs){
+        //suprise:19,20,23,24
+        android.graphics.Point p1,p2;
+        p1 = landmakrs.get(19);
+        p2 = landmakrs.get(20);
+        Log.d(TAG, "isAngry k1: "+(float)(p2.y-p1.y)/(p2.x-p1.x)+" p1: "+p1.toString()+" p2: "+p2.toString());
+        if((float)(p2.y-p1.y)/(p2.x-p1.x)<0.15){
+            return false;
+        }
+        p1 = landmakrs.get(23);
+        p2 = landmakrs.get(24);
+        Log.d(TAG, "isAngry k2: "+(float)(p2.y-p1.y)/(p2.x-p1.x)+" p1: "+p1.toString()+" p2: "+p2.toString());
+        if((float)(p2.y-p1.y)/(p2.x-p1.x)>-0.15){
+            return false;
+        }
+        return true;
+    }
+
 }
